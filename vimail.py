9ecf821beb2ad5cc57eaeb4e1db1f913860179fc
Mechaspirit1 <pabloloschi200@gmail.com>
2026-03-30 11:09:09 -0300

    Small enhancements

--- vimail.py
+++ vimail.py
@@ -1,5 +1,8 @@
+#! /usr/bin/env python3
+
 import smtplib
 from email.message import EmailMessage
+from getpass import getpass
 
 def send_test_email(sender_email, sender_password, subject, recipient_email, message):
 
@@ -17,18 +20,48 @@ def send_test_email(sender_email, sender_password, subject, recipient_email, mes
 
 
 def main():
+    
+    banner = r"""
+
+              ███                             ███  ████ 
+             ░░░                             ░░░  ░░███ 
+ █████ █████ ████  █████████████    ██████   ████  ░███ 
+░░███ ░░███ ░░███ ░░███░░███░░███  ░░░░░███ ░░███  ░███ 
+ ░███  ░███  ░███  ░███ ░███ ░███   ███████  ░███  ░███ 
+ ░░███ ███   ░███  ░███ ░███ ░███  ███░░███  ░███  ░███ 
+  ░░█████    █████ █████░███ █████░░████████ █████ █████
+   ░░░░░    ░░░░░ ░░░░░ ░░░ ░░░░░  ░░░░░░░░ ░░░░░ ░░░░░ 
+"""                                                        
+
+    print(banner)
+    print("vimail")
+    print("\033]8;;https://github.com/Mechaspirit1\033\\A tool by Pablo Loschi (Mechaspirit1)\033]8;;\033\\")
+    
     try:
         logic_sender = input("Type the address of the sender -> ").lower()
-        logic_password = input("Type the app password of the sender -> ").lower()
+        logic_password = getpass("Type the app password of the sender -> ").lower()
         email_quantity = int(input("Type the ammount of emails to be sent -> "))
         logic_message = input("Type the standard message template you wish to send -> ")
+        logic_subject = input("Type the standard message subject - > ")
 
         for i in range(email_quantity):
             logic_recipient = input("Type the address of the recipient -> ").lower()
-            send_test_email(logic_sender, logic_password, "Email Automático", logic_recipient, logic_message)
+            unique_add = input ("Type an addition to the message template -> ")
+
+            final_message = logic_message + "\n" + unique_add
+
+            send_test_email(logic_sender, logic_password, logic_subject, logic_recipient, final_message)
+            print("Transaction successful !")
+
 
     except ValueError:
-        print("Wrong value")
+        print("\nWrong value")
+        return
+    except KeyboardInterrupt:
+        print("\nProgram terminated. Exiting...")
+        return
+    except EOFError:
+        print("\nForceful termination. Exiting...")
         return
 
 if __name__ == "__main__":
